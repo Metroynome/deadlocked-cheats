adf48dfd95cdb982a5a70612948c7a951677b938
Metroynome <yort124@gmail.com>
2022-02-17 16:03:20 -0500

    Changed hook to fix freezing

--- force-spawn-pack/main.c
+++ force-spawn-pack/main.c
@@ -53,13 +53,23 @@ int GetLevel(void * GameplayFilePointer)
 
 int Active = 0;
 int SpawnedPack = 0;
+int PlayerUpdateFunc = 0;
 void SpawnPack(void)
 {
-    // Run Normal Respawn functions
-    ((void (*)())0x004EF120)();
+    // Spawn Pack if Health <= zero and if not spawned already.
+    if (*(float*)0x00235964 <= 0 && SpawnedPack == 0)
+    {
+        SpawnedPack = 1;
+        void * GameplayFilePointer = (void*)(*(u32*)0x01FFFD00);
+        int WP =  GetLevel(GameplayFilePointer);
+        int Respawn = (*(u32*)PlayerUpdateFunc) - 0x61B8; // 0x0050FAA8
+        int Pack = WP + 0xE38; // 0x004F0378
+        // Run Normal Respawn functions
+        ((void (*)())Respawn)();
 
-    // Spawn Pack
-    ((void (*)(u32))0x004F0378)(0x002F7900);
+        // Spawn Pack
+        ((void (*)(u32))Pack)(0x002F7900);
+    }
 }
 
 int main(void)
@@ -71,24 +81,25 @@ int main(void)
 	// If pointer doesn't equel Online Lobby Pointer, proceed.
 	if (GameplayFilePointer != 0x00574F88)
 	{
+        int PlayerStruct = (*(u32*)0x00225e70) - 0x430C;
+        int PlayerFunctions = (u32)PlayerStruct + 0x14;
+        PlayerUpdateFunc = (*(u32*)PlayerFunctions) + 0x34;
+        int SpawnPackHook = (*(u32*)PlayerUpdateFunc) + 0x952C; // This is 0x0051F18C
 		// Check which Level we are on.  Function returns the address of the Weapon Pack function.
 		int WeaponPack = GetLevel(GameplayFilePointer);
+
 		// If WeaponPack function isn't Zero, make it zero.
 		if (*(u32*)WeaponPack != 0)
 			*(u32*)WeaponPack = 0;
 
-        // Spawn Pack if Health <= zero and if not spawned already.
-        if (*(float*)0x00235964 <= 0 && SpawnedPack == 0)
-        {
-            SpawnedPack = 1;
-            // Hook SpawnPack
-			*(u32*)0x0051F138 = 0x0c000000 | ((u32)(&SpawnPack) / 4);
-        }
-        else if (*(float*)0x00235964 > 0 && SpawnedPack == 1)
-        {
+        // if Health is greater than zero and pack has spawned
+        if (*(float*)0x00235964 > 0 && SpawnedPack == 1)
             SpawnedPack = 0;
-        }
 
+        // Hook SpawnPack (On Outpost x12 address: 0x0051F18C)
+		*(u32*)SpawnPackHook = 0x0c000000 | ((u32)(&SpawnPack) / 4);
+
+        // DEBUG OPTIONS: L3 = Spawn Pack, R3 = Hurt Player
         if ((pad->btns & PAD_L3) == 0 && Active == 0)
 	    {
             Active = 1;
