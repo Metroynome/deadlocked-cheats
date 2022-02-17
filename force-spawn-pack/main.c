c79c02c0b313cfc44a0aa4cf02eac4d04d1a541a
Metroynome <yort124@gmail.com>
2022-02-17 16:34:56 -0500

    Removed GetLevel Function

--- force-spawn-pack/main.c
+++ force-spawn-pack/main.c
@@ -2,57 +2,9 @@
 #include <libdl/stdio.h>
 #include <libdl/pad.h>
 
-int GetLevel(void * GameplayFilePointer)
-{
-    int WeaponPack_Func = 0;
-    switch((u32)GameplayFilePointer)
-    {
-        // Outpost X12
-        case 0x0043A288:
-            WeaponPack_Func = 0x004EF540;
-            break;
-        // Korgon Outpost
-        case 0x00437EC8:
-            WeaponPack_Func = 0x004ECD58;
-            break;
-        // Metropolis
-        case 0x00437208:
-            WeaponPack_Func = 0x004EC0A8;
-            break;
-        // Blackwater City
-        case 0x00434988:
-            WeaponPack_Func = 0x004E98C0;
-            break;
-        // Command Center
-        case 0x004357C8:
-            WeaponPack_Func = 0x004E9A48;
-            break;
-        // Blackwater Docks
-        case 0x00438008:
-            WeaponPack_Func = 0x004EC288;
-            break;
-        // Aquatos Sewers
-        case 0x00437348:
-            WeaponPack_Func = 0x004EB5C8;
-            break;
-        // Marcadia Palace
-        case 0x00436C88:
-            WeaponPack_Func = 0x004EAF08;
-            break;
-        // Bakisi Isle
-        case 0x00441988:
-            WeaponPack_Func = 0x004F7BD0;
-            break;
-        // Hoven Gorge
-        case 0x00443448:
-            WeaponPack_Func = 0x004F9C28;
-            break;
-    }
-	return WeaponPack_Func;
-}
-
 int Active = 0;
 int SpawnedPack = 0;
+int PlayerStruct = 0;
 int PlayerUpdateFunc = 0;
 void SpawnPack(void)
 {
@@ -60,15 +12,13 @@ void SpawnPack(void)
     if (*(float*)0x00235964 <= 0 && SpawnedPack == 0)
     {
         SpawnedPack = 1;
-        void * GameplayFilePointer = (void*)(*(u32*)0x01FFFD00);
-        int WP =  GetLevel(GameplayFilePointer);
         int Respawn = (*(u32*)PlayerUpdateFunc) - 0x61B8; // 0x0050FAA8
-        int Pack = WP + 0xE38; // 0x004F0378
+        int Pack = (*(u32*)PlayerUpdateFunc) - 0x258E8; // 0x004F0378
         // Run Normal Respawn functions
         ((void (*)())Respawn)();
 
         // Spawn Pack
-        ((void (*)(u32))Pack)(0x002F7900);
+        ((void (*)(u32))Pack)(PlayerStruct);
     }
 }
 
@@ -81,12 +31,11 @@ int main(void)
 	// If pointer doesn't equel Online Lobby Pointer, proceed.
 	if (GameplayFilePointer != 0x00574F88)
 	{
-        int PlayerStruct = (*(u32*)0x00225e70) - 0x430C;
+        PlayerStruct = (*(u32*)0x00225e70) - 0x430C;
         int PlayerFunctions = (u32)PlayerStruct + 0x14;
         PlayerUpdateFunc = (*(u32*)PlayerFunctions) + 0x34;
-        int SpawnPackHook = (*(u32*)PlayerUpdateFunc) + 0x952C; // This is 0x0051F18C
-		// Check which Level we are on.  Function returns the address of the Weapon Pack function.
-		int WeaponPack = GetLevel(GameplayFilePointer);
+        int SpawnPackHook = (*(u32*)PlayerUpdateFunc) + 0x952C; // 0x0051F18C
+		int WeaponPack = (*(u32*)PlayerUpdateFunc) - 0x26720; // 0x004EF540
 
 		// If WeaponPack function isn't Zero, make it zero.
 		if (*(u32*)WeaponPack != 0)
@@ -100,24 +49,24 @@ int main(void)
 		*(u32*)SpawnPackHook = 0x0c000000 | ((u32)(&SpawnPack) / 4);
 
         // DEBUG OPTIONS: L3 = Spawn Pack, R3 = Hurt Player
-        if ((pad->btns & PAD_L3) == 0 && Active == 0)
-	    {
-            Active = 1;
-            // Outpost X12 only currently
-            ((void (*)(u32))0x004F0378)(0x002F7900);
-        }
-        else if ((pad->btns & PAD_R3) == 0 && Active == 0)
-	    {
-            Active = 1;
-            // Outpost X12 only currently
-            // ((void (*)(u32))0x004F0378)(0x002F7900);
-            // Hurt Player
-            ((void (*)(u32, u16, u16))0x00502658)(0x002F7900, 5, 0);
-        }
-        if (!(pad->btns & PAD_L3) == 0 && !(pad->btns & PAD_R3) == 0)
-        {
-            Active = 0;
-        }
+        // if ((pad->btns & PAD_L3) == 0 && Active == 0)
+	    // {
+        //     Active = 1;
+        //     // Outpost X12 only currently
+        //     ((void (*)(u32))0x004F0378)(0x002F7900);
+        // }
+        // else if ((pad->btns & PAD_R3) == 0 && Active == 0)
+	    // {
+        //     Active = 1;
+        //     // Outpost X12 only currently
+        //     // ((void (*)(u32))0x004F0378)(0x002F7900);
+        //     // Hurt Player
+        //     ((void (*)(u32, u16, u16))0x00502658)(0x002F7900, 5, 0);
+        // }
+        // if (!(pad->btns & PAD_L3) == 0 && !(pad->btns & PAD_R3) == 0)
+        // {
+        //     Active = 0;
+        // }
 	}
 	return 0;
 }
