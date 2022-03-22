6fff18b8407efe41868f4e530b5120a580ed3f59
Metroynome <yort124@gmail.com>
2022-03-22 19:42:26 -0400

    Added Dpad Support for Camera Speed in game

--- mod-menu/main.c
+++ mod-menu/main.c
@@ -1901,18 +1901,61 @@ void UnlimitedAmmo()
 ================      Camera Speed
 ========
 \*========================================================*/
+void DPadCameraLogic(void)
+{
+	int CameraSpeed = 0x100;
+
+	int CameraSpeedAddr = 0x00171F18;
+	int num = 0;
+	if ((*(u16*)0x001EE682 & PAD_LEFT) == 0)
+	{
+		num = -0x8;
+	}
+	else if ((*(u16*)0x001EE682 & PAD_RIGHT) == 0 | (*(u16*)0x001EE682 & PAD_CROSS) == 0)
+	{
+		num = 0x8;
+	}
+	int NewSpeed = (u32)(*(u32*)CameraSpeedAddr + num);
+	if (NewSpeed >= CameraSpeed)
+	{
+		NewSpeed = CameraSpeed;
+	}
+	else if (NewSpeed <= 0)
+	{
+		NewSpeed = 0;
+	}
+	*(u32*)CameraSpeedAddr = NewSpeed;
+}
 void patchCameraSpeed()
 {
 	const u16 SPEED = 0x100;
 	char buffer[16];
 
-	// Check if the value is the default max of 64
-	// This is to ensure that we only write here when
-	// we're in game and the patch hasn't already been applied
-	if (CAMERA_SPEED_PATCH_OFF1 == 0x40)
+	if (gameIsIn())
 	{
-		CAMERA_SPEED_PATCH_OFF1 = SPEED;
-		CAMERA_SPEED_PATCH_OFF2 = SPEED+1;
+		// Which Start Menu option is Selected
+		int StartMenuSelection = *(u32*)0x0030DE40;
+		// if Camera Speed Option is Selected, Patch Menu
+		if (StartMenuSelection == 3 && *(u32*)0x00389CA8 != 0x00548F7C)
+		{
+			*(u32*)0x00389CA8 = 0x00548F7C; // d-pad Left
+			*(u32*)0x00389CAC = 0x00548F7C; // d-pad Right
+		}
+		// if Camera Speed Option is not Selected, Unpatch Menu
+		else if (StartMenuSelection != 3 && *(u32*)0x00389CA8 != 0x00548FDC)
+		{
+			*(u32*)0x00389CA8 = 0x00548FDC; // d-pad Left
+			*(u32*)0x00389CAC = 0x00548FDC; // d-pad Right
+		}
+		// Patch Camera Speed Logic with mine.
+		if (*(u32*)0x00561BCC == 0x14460003)
+		{
+			*(u32*)0x00561BCC = 0x0C000000 | ((u32)(&DPadCameraLogic) / 4);
+			*(u32*)0x00561BD0 = 0;
+			*(u32*)0x00561BD8 = 0;
+			*(u32*)0x00561C1C = 0x3C050017;
+			*(u32*)0x00561C20 = 0x8CA21F18;
+		}
 	}
 
 	// Patch edit profile bar
