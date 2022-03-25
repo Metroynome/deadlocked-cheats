3f31322f6ffa79ed85c1426aa089f6859cce7680
Metroynome <yort124@gmail.com>
2022-03-24 22:50:56 -0400

    Updated Assembly.  still not perfect. :(

--- dpad-camera-speed/main.c
+++ dpad-camera-speed/main.c
@@ -4,30 +4,84 @@
 #include <libdl/player.h>
 #include <libdl/pad.h>
 
-int CameraSpeed = 0x100;
-
 void DPadCameraLogic(void)
 {
-	int CameraSpeedAddr = 0x00171F18;
-	int num = 0;
-	if ((*(u16*)0x001EE682 & PAD_LEFT) == 0)
-	{
-		num = -0x8;
-	}
-	else if ((*(u16*)0x001EE682 & PAD_RIGHT) == 0 | (*(u16*)0x001EE682 & PAD_CROSS) == 0)
-	{
-		num = 0x8;
-	}
-	int NewSpeed = (u32)(*(u32*)CameraSpeedAddr + num);
-	if (NewSpeed >= CameraSpeed)
-	{
-		NewSpeed = CameraSpeed;
-	}
-	else if (NewSpeed <= 0)
-	{
-		NewSpeed = 0;
-	}
-	*(u32*)CameraSpeedAddr = NewSpeed;
+	asm (
+	"	lui	    $t6,0x001F;"
+	"	lhu	    $t6,-0x197E($t6);"
+	"	andi	$t7,$t6,0x0080;"
+	"   addiu	$t5,$zero,-0x8;"
+	"	beq	    $t7,$zero,_JUMPHERE;"
+	"	andi	$t7,$t6,0x4020;"
+	"	addiu	$t5,$zero,0x8;"
+	"	xori	$t6,$t7,0x4020;"
+	"	movz	$t5,$zero,$t6;"
+	"_JUMPHERE:"
+	"	addiu	$t8,$zero,0x40;"
+	"	addu	$t9,$v0,$t5;"
+	"	slti	$t1,$t9,0x0040;"
+	"	slt	    $t5,$zero,$t9;"
+	"	movz	$t9,$zero,$t5;"
+	"	xori	$t4,$t1,0x0000;"
+	"	movz	$t9,$t8,$t4;"
+	"   sw	    $t9,0x0($a0);"
+	"	jr	    $ra;"
+	);
+	// v0: Loaded Camera Speed
+	// a0: Camera speed Address
+	// a2: Max Camera speed
+
+
+
+	// asm (
+		// DOESNT GO TO 100 IF ON 78 >:(
+	// 	// Load Pad 1
+	// 	"lui	$t6, 0x001F\n"
+	// 	"addiu 	$t6, $t6, 0xE682\n"
+	// 	"lh	$t6, 0x0($t6)\n"
+	// 	// Load Jokers
+	// 	"addiu 	$t7, $zero, 0xFF7F\n" // Left
+	// 	"addiu 	$t8, $zero, 0xFFDF\n" // Right
+	// 	"addiu 	$t9, $zero, 0xBFFF\n" // Cross
+	// 	// if d-pad Left, Subtract
+	// 	"addiu 	$t5, $zero, -0x8\n"
+	// 	"beq 	$t6, $t7, _DoNewSpeed\n"
+	// 	// if d-pad Right, Add
+	// 	"addiu 	$t5, $zero, 0x8\n"
+	// 	"beq 	$t6, $t8, _DoNewSpeed\n"
+	// 	//if Cross, Add
+	// 	"addiu 	$t5, $zero, 0x8\n"
+	// 	"beq 	$t6, $t9, _DoNewSpeed\n"
+
+
+	// 	"_DoNewSpeed:"
+	// 	"add 	$t5, $t5, $v0\n"
+	// 	// if less than zero, save Max Speed
+	// 	"addiu 	$t9, $zero, -0x1\n"
+	// 	"slt	$t6, $t9, $t5\n"
+	// 	"nop\n"
+	// 	"beq 	$t6, $zero, _GoToMax\n"
+
+	// 	"addiu 	$t9, $a2, 0x1\n"
+	// 	"slt 	$t6, $t9, $t5\n"
+	// 	"nop\n"
+	// 	"bne 	$t6, $zero, _GoToZero\n"
+
+	// 	"add	$t5, $zero, $t5\n"
+	// 	"beq	$zero, $zero, _exitDpadCamera\n"
+
+	// 	"_GoToMax:"
+	// 	"add	$t5, $zero, $a2\n"
+	// 	"beq	$zero, $zero, _exitDpadCamera\n"
+
+	// 	"_GoToZero:"
+	// 	"add	$t5, $zero, $zero\n"
+
+	// 	// Everything Else!
+	// 	"_exitDpadCamera:"
+	// 	"sw 	$t5, 0x0($a0)\n"
+	// 	"jr 	$ra\n"
+	// );
 }
 
 int main(void)
@@ -54,8 +108,6 @@ int main(void)
 			*(u32*)0x00561BCC = 0x0C000000 | ((u32)(&DPadCameraLogic) / 4);
 			*(u32*)0x00561BD0 = 0;
 			*(u32*)0x00561BD8 = 0;
-			*(u32*)0x00561C1C = 0x3C050017;
-			*(u32*)0x00561C20 = 0x8CA21F18;
 		}
 	}
 	return 0;
