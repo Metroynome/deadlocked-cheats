c91fc5764f5b144286e37e78b957ece91c3285be
Metroynome <yort124@gmail.com>
2023-02-11 13:40:19 -0500

    Added Dual Wield

---
+++ dualwield/main.c
@@ -0,0 +1,44 @@
+#include <tamtypes.h>
+#include <libdl/stdio.h>
+#include <libdl/game.h>
+#include <libdl/player.h>
+
+int DualWieldWeapons()
+{
+	// v1 = current weapon
+	// v0 = vipers
+	register unsigned short WeaponID asm("v1");
+	register unsigned short DualWieldIDs asm("v0");
+	// If WeaponID = Vipers/Mag/Fusion, continue.
+	if (WeaponID == 0x1094 || WeaponID == 0x1087 || WeaponID == 0x1096)
+	{
+		return DualWieldIDs = WeaponID;
+	}
+}
+
+int main(void)
+{
+	if (!isInGame())
+		return -1;
+	
+	// Fix Dual Fusion not shooting dual viper shots out of left hand.
+	// int p = 0x00347aa0;
+	// int GadgetMoby = *(u32*)(p + 0x22F4);
+	// if (GadgetMoby != 0)
+	// {
+	// 	int AnotherPointer = *(u32*)(GadgetMoby + 0xAC);
+	// 	*(u32*)(AnotherPointer + 0x48) = p;
+	// }
+
+	// Dual Wield Vipers or Magma Cannon
+	// Hook our function
+	*(u32*)0x005DDA3C = 0x0c000000 | ((u32)(&DualWieldWeapons) >> 2);
+	*(u32*)0x005DDA40 = 0x87A30020; // loads weapon ID into v1.
+	// Set Second HAnd weapon to needed dual wield weapon.
+	*(u32*)0x005DDA54 = 0x0040202D;
+	// Constant Write which Weapon PVars to get if Magma Cannon is selected
+	if (*(u32*)0x00349f50 == 3)
+		*(u32*)0x00349DB8 = 2;
+	
+	return 0;
+}
