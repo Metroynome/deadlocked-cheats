da21deca25797aff638ae87455ae8f9acb7ff429
Metroynome <yort124@gmail.com>
2023-10-24 12:00:15 -0400

    moreflappy

--- flappy-hovership/main.c
+++ flappy-hovership/main.c
@@ -1,15 +1,27 @@
 #include <tamtypes.h>
+#include <libdl/dl.h>
+#include <libdl/player.h>
+#include <libdl/pad.h>
 #include <libdl/stdio.h>
 #include <libdl/game.h>
-#include <libdl/player.h>
 #include <libdl/moby.h>
 #include <libdl/utils.h>
 #include <libdl/math.h>
 #include <libdl/math3d.h>
 #include <libdl/vehicle.h>
 
-void doTheFlapFlap(a0, a1)
+void doTheFlapFlap(void)
 {
+	// use x or not to flap!
+	int Manual = 0;
+
+	static float negativef = -1;
+	static float positivef = 0.50;
+	static float rotModifier = 0.10;
+	static int state = 0;
+	static float lRot = 0;
+	static float rRot = 0;
+
 	int i;
 	Player ** players = playerGetAll();
 	for (i = 0; i < GAME_MAX_PLAYERS; ++i) {
@@ -25,42 +37,58 @@ void doTheFlapFlap(a0, a1)
 		u32 wing1 = ((u32)moby->PVar + 0x3f0);
 		u32 wing2 = ((u32)moby->PVar + 0x430);
 
-		float negativef = -0.75;
-		float positivef = 0.75;
-		float rotModifier = 0.05;
-		static int state = 0;
-		float left = 0;
-		float right = 0;
+		float left_wing = *(float*)wing1;
+		float right_wing = *(float*)wing2;
 
-		float left_wing_start = *(float*)wing1;
-		float right_wing_start = *(float*)wing2;
-		if (state == 0) {
-			if (left_wing_start < positivef){
-				left = left_wing_start + rotModifier;
-				right = right_wing_start - rotModifier;
-			} else {
-				state = 1;
+		if (!Manual) {
+			if (state == 0) {
+				if (left_wing < positivef) {
+					lRot = left_wing + rotModifier;
+					rRot = right_wing - rotModifier;
+				} else {
+					state = 1;
+				}
 			}
-		}
-		if (state == 1){
-			if (left_wing_start > negativef){
-				left = left_wing_start - rotModifier;
-				right = right_wing_start + rotModifier;
-			} else {
-				state = 2;
+			if (state == 1) {
+				if (left_wing > negativef) {
+					lRot = left_wing - rotModifier;
+					rRot = right_wing + rotModifier;
+				} else {
+					state = 2;
+				}
 			}
-		}
-		if (state == 2){
-			if (left_wing_start < 0){
-				left = left_wing_start + rotModifier;
-				right = right_wing_start - rotModifier;
-			} else {
-				state = 0;
+			if (state == 2) {
+				if (left_wing < 0) {
+					lRot = left_wing + rotModifier;
+					rRot = right_wing - rotModifier;
+				} else {
+					state = 0;
+				}
+			}
+		} else {
+			// PadButtonStatus * pad = (PadButtonStatus*)player->Paddata;
+			if (state == 0) {
+				if (playerPadGetButtonDown(player, PAD_CROSS) > 0) {
+					if (left_wing > positivef) {
+						lRot = left_wing + rotModifier;
+						rRot = right_wing - rotModifier;
+					}
+				} else if (playerPadGetButtonUp(player, PAD_CROSS) > 0) {
+					state = 1;
+				}
+			}
+			if (state == 1) {
+				if (left_wing < 0) {
+					lRot = left_wing - rotModifier;
+					rRot = right_wing + rotModifier;
+				} else {
+					state = 0;
+				}
 			}
 		}
 		
-		*(float*)wing1 = left;
-		*(float*)wing2 = right;
+		*(float*)wing1 = lRot;
+		*(float*)wing2 = rRot;
 	}
 }
 
@@ -76,10 +104,14 @@ void hookTheFlapFlap(void)
 
 int main(void)
 {
+	dlPreUpdate();
+
 	if (!isInGame())
 		return -1;
 	
 	hookTheFlapFlap();
+
+	dlPostUpdate();
 
 	return 0;
 }
