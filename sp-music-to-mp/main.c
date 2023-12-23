3b08cbdb17ac8186f926e477d0612ab4e1478c3b
Metroynome <yort124@gmail.com>
2023-12-23 17:16:40 -0500

    Updated

--- sp-music-to-mp/main.c
+++ sp-music-to-mp/main.c
@@ -10,8 +10,8 @@
 #include <libdl/pad.h>
 #include <libdl/music.h>
 
-#define TRACK_RANGE_MIN (*(u8*)0x0021EC08)
-#define TRACK_RANGE_MAX (*(u8*)0x0021EC0C)
+#define TRACK_RANGE_MIN (0x0021EC08)
+#define TRACK_RANGE_MAX (0x0021EC0C)
 #define CURRENT_TRACK (*(u16*)0x00206990)
 #define TRACK_DURATION (*(u32*)0x002069A4)
 
@@ -62,13 +62,12 @@ void CampaignMusic(void)
 	static int SetupMusic = 0;
 	static short Music[0x1f][2] = {};
 
-	// check to see if multiplayer tracks are loaded
-	if (!musicIsLoaded())
+	u32 CodeSegmentPointer = *(u32*)0x0021DA24;
+	if (!musicIsLoaded() || CodeSegmentPointer == 0x01430700 || !enableSingleplayerMusic)
 		return;
 
 	u32 NewTracksLocation = 0x001CF940;
-	if (!FinishedConvertingTracks)
-	{
+	if (!FinishedConvertingTracks) {
 		AddedTracks = 0;
 		int MultiplayerSector = *(u32*)0x001CF85C;
 		int Stack = 0x0023ac00;
@@ -76,28 +75,22 @@ void CampaignMusic(void)
 		int a;
 		int Offset = 0;
 
-		// Zero out stack by the appropriate heap size (0x2a0 in this case)
-		// This makes sure we get the correct values we need later on.
+		// Zero out stack by the appropriate heap size
 		memset((u32*)Stack, 0, 0x2A0);
 
 		// Loop through each WAD ID
-		for(a = 0; a < 12; a++)
-		{
+		for(a = 0; a < 12; a++) {
 			Offset += 0x18;
 			int WAD = *(u32*)(WAD_Table + Offset);
 			// Check if Map Sector is not zero
-			if (WAD != 0)
-			{
+			if (WAD != 0) {
 				internal_wadGetSectors(WAD, 1, Stack);
 				int WAD_Sector = *(u32*)(Stack + 0x4);
 
 				// make sure WAD_Sector isn't zero
-				if (WAD_Sector != 0)
-				{
-					printf("WAD: 0x%X\n", WAD);
-					printf("WAD Sector: 0x%X\n", WAD_Sector);
-
-					// do music stuffs~
+				if (WAD_Sector != 0) {
+					DPRINTF("WAD: 0x%X\n", WAD);
+					DPRINTF("WAD Sector: 0x%X\n", WAD_Sector);
 					// Get SP 2 MP Offset for current WAD_Sector.
 					int SP2MP = WAD_Sector - MultiplayerSector;
 					// Remember we skip the first track because it is the start of the sp track, not the body of it.
@@ -118,26 +111,15 @@ void CampaignMusic(void)
 						Music[AddedTracks][1] = (u16)ConvertedTrack_RightAudio;
 						// If on DreadZone Station, and first song, add 0x20 instead of 0x20
 						// This fixes an offset bug.
-						if (a == 0 && b == 0)
-						{
-							b += 0x28;
-						}
-						else
-						{
-							b += 0x20;
-						}
+						b = (a == 0 && b == 0) ? b + 0x28 : b + 0x20;
 						AddedTracks++;
 					}
 					while (*(u32*)(Songs + b) != 0);
-				}
-				else
-				{
+				} else {
 					Offset -= 0x18;
 					a--;
 				}
-			}
-			else
-			{
+			} else {
 				a--;
 			}
 		// Zero out stack to finish the job.
@@ -145,94 +127,68 @@ void CampaignMusic(void)
 		}
 
 		FinishedConvertingTracks = 1;
-		printf("Added Tracks: %d\n", AddedTracks);
+		DPRINTF("Added Tracks: %d\n", AddedTracks);
 	};
 	
 
 	int DefaultMultiplayerTracks = 0x0d; // This number will never change
-	int StartingTrack = musicTrackRangeMin();
+	int StartingTrack = *(u32*)musicTrackRangeMin();
 	int AllTracks = DefaultMultiplayerTracks + AddedTracks;
 	int TotalTracks = (DefaultMultiplayerTracks - StartingTrack + 1) + AddedTracks;
-	int CodeSegmentPointer = *(u32*)0x0021DA24;
-	// If not in main lobby, game lobby, ect.
-	if(CodeSegmentPointer != 0x01430700){
 		// if TRACK_RANGE_MAX doesn't equal TotalTracks
-		if(musicTrackRangeMax() != TotalTracks){
-			int MusicFunctionData = CodeSegmentPointer + 0x28A0D4;
-			*(u16*)MusicFunctionData = AllTracks;
-		}
-		if (*(u32*)NewTracksLocation == 0 || !SetupMusic)
-		{
-			int Track;
-			for(Track = 0; Track < AddedTracks; ++Track)
-			{
-				u32 Left = (u16)Music[Track][0];
-				u32 Right = (u16)Music[Track][1];
-				u32 Add1 = 0x10000;
-				u32 Add2 = 0x20000;
-				u32 Add3 = 0x30000;
-				if (Track == 8)
-				{
-					Right += Add1;
-				}
-				else if (Track >= 9 && Track <= 31)
-				{
-					Left += Add1;
-					Right += Add1;
-				}
-				else if (Track == 32)
-				{
-					Left += Add1;
-					Right += Add2;
-				}
-				else if (Track >= 33 && Track <= 56)
-				{
-					Left += Add2;
-					Right += Add2;
-				}
-				else if (Track >= 57)
-				{
-					Left += Add3;
-					Right += Add3;
-				}
-				
-				*(u32*)(NewTracksLocation) = Left;
-				*(u32*)(NewTracksLocation + 0x08) = Right;
-				NewTracksLocation += 0x10;
+	if (*(u32*)musicTrackRangeMax() != TotalTracks) {
+		int MusicFunctionData = CodeSegmentPointer + 0x28A0D4;
+		*(u16*)MusicFunctionData = AllTracks;
+	}
+	if (*(u32*)NewTracksLocation == 0 || !SetupMusic) {
+		int Track;
+		for(Track = 0; Track < AddedTracks; ++Track) {
+			u32 Left = (u16)Music[Track][0];
+			u32 Right = (u16)Music[Track][1];
+			u32 Add1 = 0x10000;
+			u32 Add2 = 0x20000;
+			u32 Add3 = 0x30000;
+			if (Track == 8) {
+				Right += Add1;
+			} else if (Track >= 9 && Track <= 31) {
+				Left += Add1;
+				Right += Add1;
+			} else if (Track == 32) {
+				Left += Add1;
+				Right += Add2;
+			} else if (Track >= 33 && Track <= 56) {
+				Left += Add2;
+				Right += Add2;
+			} else if (Track >= 57) {
+				Left += Add3;
+				Right += Add3;
 			}
-			SetupMusic = 1;
+			
+			*(u32*)(NewTracksLocation) = Left;
+			*(u32*)(NewTracksLocation + 0x08) = Right;
+			NewTracksLocation += 0x10;
 		}
+		SetupMusic = 1;
 	}
 
 	// If in game
-	if(isInGame())
-	{
+	if (isInGame()) {
 		// if Cheats are Active
-		if (enableSingleplayerMusic)
-		{
+		if (enableSingleplayerMusic) {
 			// if TRACK_MAX_RANGE doesn't equal TotalTracks
 			// This will only happen in game if all codes are turned off and back on.
-			if (*(u32*)0x0021EC0C != TotalTracks)
-			{
-				*(u32*)0x0021EC0C = TotalTracks;
-			}
-		}
-		else
-		{
+			if (*(u32*)musicTrackRangeMax() != TotalTracks)
+				*(u32*)musicTrackRangeMax() = TotalTracks;
+		} else {
 			// Reset number of tracks to play to original 10.
-			if (*(u32*)0x0021EC0C != 0x0a)
-			{
-				*(u32*)0x0021EC0C = 0x0a;
-			}
+			if (*(u32*)musicTrackRangeMax() != 0x0a)
+				*(u32*)musicTrackRangeMax() = 0x0a;
 		}
-		if (*(u32*)0x002069A0 <= 0)
-		{
+		if (*(u32*)0x002069A0 <= 0) {
 			int CurrentTrack = musicCurrentTrack();
 			int TrackDuration = musicTrackDuration();
 			if ((CurrentTrack > DefaultMultiplayerTracks * 2) && (CurrentTrack != -1 && *(u32*)0x0020698C != 0) && (TrackDuration <= 0x3000))
-			{
 				musicTransitionTrack(0,0,0,0);
-			}
 		}
 	}
 }
