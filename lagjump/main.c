5087e55c1e6ba57158883c3a6fb7676229578e6e
Metroynome <yort124@gmail.com>
2025-06-15 18:12:43 -0400

    Removed unessasary code

--- lagjump/main.c
+++ lagjump/main.c
@@ -11,31 +11,34 @@
 #include <libdl/vehicle.h>
 #include <libdl/time.h>
 
-int patchCrouchJumpTransitions(Player *this)
-{
-	// call original CrouchJumpTransitions function
-	return ((int (*)(Player* ))0x005efd20)(this);
-}
+// int patchCrouchJumpTransitions(Player *this)
+// {
+// 	// call original CrouchJumpTransitions function
+// 	return ((int (*)(Player* ))0x005efd20)(this);
+// }
 
 void lagjump(void)
 {
-	int hook = 0x006095b8;
-	if (*(u32*)hook == 0x0c17bf48) {
-		// hook our custom crouchjumptransitions logic
-		HOOK_JAL(hook, &patchCrouchJumpTransitions);
-		// disable "LookmodeTransition" in "InitBodyState"
-		POKE_U32(0x00606aa0, 0);
-	}
-
-	// force player to crouch if R2 is pressed
-	Player *player = playerGetFromSlot(0);
-	PlayerVTable *vtable = playerGetVTable(player);
-	if (playerPadGetButtonDown(player, PAD_R2 | PAD_CROSS) > 0) {
-		float stickStrength = *(float*)((u32)player + 0x2e08);
-		int jumpDir = ((int (*)(Player *))0x005ee3b8)(player);
-		short int onGood = *(short*)((u32)player + 0x2f0);
-		if (0.9 < stickStrength && onGood && jumpDir != 2) {
-			vtable->UpdateState(player, 0xb, 1, 0, 1);
+	// int hook = 0x006095b8;
+	// if (*(u32*)hook == 0x0c17bf48) {
+	// 	// hook our custom crouchjumptransitions logic
+	// 	HOOK_JAL(hook, &patchCrouchJumpTransitions);
+	// 	// disable "LookmodeTransition" in "InitBodyState"
+	// 	POKE_U32(0x00606aa0, 0);
+	// }
+	int i;
+	for (i = 0; i < GAME_MAX_PLAYERS; ++i) {
+		Player *player = playerGetFromSlot(i);
+		if (player && player->IsLocal) {
+			PlayerVTable *vtable = playerGetVTable(player);
+			if (playerPadGetButtonDown(player, PAD_R2 | PAD_CROSS) > 0) {
+				float stickStrength = *(float*)((u32)player + 0x2e08);
+				int jumpDir = ((int (*)(Player *))0x005ee3b8)(player);
+				short int onGood = *(short*)((u32)player + 0x2f0);
+				if (0.9 < stickStrength && onGood && jumpDir != 2) {
+					vtable->UpdateState(player, 0xb, 1, 0, 1);
+				}
+			}
 		}
 	}
 }
