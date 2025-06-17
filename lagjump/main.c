1106a4b4a91a471d4cbf86dbb329ebaa8f4b487e
Metroynome <yort124@gmail.com>
2025-06-16 21:28:15 -0400

    updated

--- lagjump/main.c
+++ lagjump/main.c
@@ -11,33 +11,41 @@
 #include <libdl/vehicle.h>
 #include <libdl/time.h>
 
-// int patchCrouchJumpTransitions(Player *this)
-// {
-// 	// call original CrouchJumpTransitions function
-// 	return ((int (*)(Player* ))0x005efd20)(this);
-// }
-
 void lagjump(void)
 {
-	// int hook = 0x006095b8;
-	// if (*(u32*)hook == 0x0c17bf48) {
-	// 	// hook our custom crouchjumptransitions logic
-	// 	HOOK_JAL(hook, &patchCrouchJumpTransitions);
-	// 	// disable "LookmodeTransition" in "InitBodyState"
-	// 	POKE_U32(0x00606aa0, 0);
-	// }
+	if (!isInGame()) return;
+
+	static volatile int state[2] = {0,0};
+
+	//Player* lp = playerGetFromSlot(0);
+	//char buf[64];
+	//snprintf(buf, sizeof(buf), "%d", lp->PlayerState);
+	//gfxHelperDrawText(15, SCREEN_HEIGHT - 15, 0, 0, 1, 0x80FFFFFF, buf, -1, TEXT_ALIGN_BOTTOMLEFT, COMMON_DZO_DRAW_NORMAL);
+
 	int i;
-	for (i = 0; i < GAME_MAX_PLAYERS; ++i) {
+	for (i = 0; i < 2; ++i) {
 		Player *player = playerGetFromSlot(i);
-		if (player && player->IsLocal) {
+		if (player && player->PlayerMoby) {
+			int hasState = player->PlayerState == PLAYER_STATE_SKID
+			|| player->PlayerState == PLAYER_STATE_WALK
+			|| player->PlayerState == PLAYER_STATE_IDLE
+			|| player->PlayerState == PLAYER_STATE_FALL
+			//|| player->PlayerState == PLAYER_STATE_FLIP_JUMP
+			;
 			PlayerVTable *vtable = playerGetVTable(player);
-			if (playerPadGetButtonDown(player, PAD_R2 | PAD_CROSS) > 0) {
-				float stickStrength = *(float*)((u32)player + 0x2e08);
-				int jumpDir = ((int (*)(Player *))0x005ee3b8)(player);
-				short int onGood = *(short*)((u32)player + 0x2f0);
-				if (0.9 < stickStrength && onGood && jumpDir != 2) {
-					vtable->UpdateState(player, 0xb, 1, 0, 1);
+			float stickStrength = *(float*)((u32)player + 0x2e08);
+			int jumpDir = ((int (*)(Player *))0x005ee3b8)(player);
+			if (playerPadGetButton(player, PAD_R2 | PAD_CROSS) > 0) {
+				if (hasState && state[i] < 4) {
+					if (0.9 < stickStrength && player->Ground.onGood && jumpDir != 2) {
+						vtable->UpdateState(player, 0xb, 1, 0, 1);
+					}
+				} else {
+					state[i]++;
 				}
+			//printf("%08X state:%d jumpDir:%d stick:%.1f ongood:%d pad:%d stategood:%d\n", gameGetTime(), player->PlayerState, jumpDir, stickStrength, player->Ground.onGood, state[i], hasState);
+			} else {
+				state[i] = 0;
 			}
 		}
 	}
@@ -47,9 +55,7 @@ int main(void)
 {
 	dlPreUpdate();
 
-	if (isInGame()) {
-		lagjump();
-	}
+	lagjump();
 
 	dlPostUpdate();
 
